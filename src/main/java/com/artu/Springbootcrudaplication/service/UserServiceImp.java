bd039e300be4f943d0f2f1492bc744effe63c693
tupcha <terem.tema2@gmail.com>
2022-05-26 13:48:30 +0300

    1# remove @Autowired 2# brought back the UserService interface 3# Add @Transactional

--- src/main/java/com/artu/Springbootcrudaplication/service/UserServiceImp.java
+++ src/main/java/com/artu/Springbootcrudaplication/service/UserServiceImp.java
@@ -4,6 +4,7 @@ import com.artu.Springbootcrudaplication.model.User;
 import com.artu.Springbootcrudaplication.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -11,11 +12,12 @@ import java.util.List;
 public class UserServiceImp implements UserService {
     private final UserRepository userRepository;
 
-    @Autowired
+
     public UserServiceImp(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
+    @Transactional
     public User findById(Long id) {
         return userRepository.getOne(id);
     }
@@ -24,10 +26,12 @@ public class UserServiceImp implements UserService {
         return userRepository.findAll();
     }
 
+    @Transactional
     public User saveUser(User user) {
         return userRepository.save(user);
     }
 
+    @Transactional
     public void deletedById(Long id) {
         userRepository.deleteById(id);
     }
